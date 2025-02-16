63f8efec35bdfdacdfb9de0f0db27ea6a0e630da
Eduardo S. Libardi <eslibardi@gmail.com>
2025-02-16 12:13:48 -0300

    Project complete with InboxScreen, mocks and play

--- taskbox/.storybook/preview.js
+++ taskbox/.storybook/preview.js
@@ -1,5 +1,11 @@
 import '../src/index.css';
 
+// Registers the msw addon
+import { initialize, mswLoader } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize();
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -10,6 +16,7 @@ const preview = {
       },
     },
   },
+  loaders: [mswLoader],
 };
 
 export default preview;
